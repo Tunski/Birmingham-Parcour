41e29aa2ec5069f2ab675f7782b8d718cab932ed
Patrick Sullivan <patdsully88@gmail.com>
2015-02-21 12:29:52 -0600

    yo generated angular web app

---
+++ client/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name birminghamParcourApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the birminghamParcourApp
+ */
+angular.module('birminghamParcourApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
