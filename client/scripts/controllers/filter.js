b7bc2b9973250b3730fbbbadca4e76e1b1d4e58c
Braden Groom <braden.groom@gmail.com>
2015-02-21 15:22:11 -0600

    adding work on admin pages

--- client/scripts/controllers/filter.js
+++ client/scripts/controllers/filter.js
@@ -8,10 +8,61 @@
  * Controller of the birminghamParcourApp
  */
 angular.module('birminghamParcourApp')
-    .controller('FilterCtrl', function ($scope) {
-        $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    .controller('FilterCtrl', function ($scope, $location) {
+
+        $scope.results = getResults();
+
+        function getResults() {
+            return [
+                {
+                    name: "Trail 1",
+                    id: 100
+                }, {
+                    name: "Trail 2",
+                    id: 101
+                }, {
+                    name: "Trail 3",
+                    id: 102
+                }, {
+                    name: "Trail 4",
+                    id: 103
+                }, {
+                    name: "Trail 5",
+                    id: 104
+                }, {
+                    name: "Trail 6",
+                    id: 105
+                }, {
+                    name: "Trail 7",
+                    id: 106
+                }, {
+                    name: "Trail 8",
+                    id: 107
+                }, {
+                    name: "Trail 9",
+                    id: 108
+                }, {
+                    name: "Trail 10",
+                    id: 109
+                }];
+        }
+
+        $scope.showMap = function () {
+            var checkedResults = getCheckedResults();
+            var allResults = $scope.results.map(function(result) {
+                return result.id;
+            });
+            $location.path("/admin/clean").search({checked: checkedResults, results: allResults});
+        }
+        
+        function getCheckedResults() {
+            var checked = [];
+            
+            $('.result-checkbox:checked').each(function() {
+                    checked.push($(this).attr('value'));
+            });
+            
+            return checked.join(",");
+        }
+
     });
