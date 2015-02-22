7bc250ddea8d1114cacc4d02c739735da04f017a
Patrick Sullivan <patdsully88@gmail.com>
2015-02-22 09:15:41 -0600

    ba baam

--- client/scripts/controllers/searchCtrl.js
+++ client/scripts/controllers/searchCtrl.js
@@ -15,24 +15,21 @@ angular.module('birminghamParcourApp')
   .controller('searchCtrl', function ($scope,$geolocation) {
 
     $scope.searchResults = [];
-
     $scope.searchText = null;
-
     $scope.markers = {};
     $scope.myLocation = { 'lat': 33.512, 'lng': -86.808, 'zoom': 12 };
-
     $scope.myCoords = $geolocation.position.coords; // this is regularly updated
     $scope.myError = $geolocation.position.error; // this becomes truthy, and has 'code' and 'message' if an error occurs
-    //
-    //
+
 
     $scope.searchCurrentLocation = function(){
-
+      console.log('searchCurrentLocation');
       $scope.searchText = "Current Location";
       $geolocation.getCurrentPosition({
         timeout: 60000
       }).then(function(data){
 
+        console.log('current location:',data.coords);
         $scope.myLocation = {
           lat: data.coords.latitude,
           lng: data.coords.longitude,
@@ -54,8 +51,12 @@ angular.module('birminghamParcourApp')
     }
 
     $scope.selectItem = function(item){
-
+        console.log('item selected', item);
     }
+
+
+    //init the controller with the current location
+    $scope.searchCurrentLocation();
 
   });
 
